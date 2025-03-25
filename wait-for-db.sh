0f69e53922f1457d6c7931c2d624ce33ae2b1021
Matheus <matheus081105@gmail.com>
2025-03-24 23:30:00 -0300

    .

--- wait-for-db.sh
+++
@@ -1,6 +0,0 @@
-until mysql -h "$DB_HOST" -u "$DB_USERNAME" -p"$DB_PASSWORD" "$DB_NAME" -e 'select 1'; do
-  echo "Waiting for database to be ready..."
-  sleep 2
-done
-echo "Database is ready!"
-exec "$@"
